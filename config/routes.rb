6726b29d411c69f165aa9b738e8191c7a51fb0fd
aribrashear <aribrashear@gmail.com>
2022-10-12 14:42:04 -0700

    :tada: Finished index functionality.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+get 'posts' => 'post#index', as: 'posts'
+get 'post/:id' => 'post#show', as: 'post'
+root "post#index"
 end
