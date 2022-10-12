c8a63d0a2ae8874acb0cfea270df3e4f043fec00
aribrashear <aribrashear@gmail.com>
2022-10-12 15:45:45 -0700

    :boom: Finished blog-create functionality.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 get 'posts' => 'post#index', as: 'posts'
+post 'posts' => 'post#create'
 get 'posts/new' => 'post#new', as: 'new_post'
 get 'post/:id' => 'post#show', as: 'post'
 root "post#index"
