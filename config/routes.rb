b884ba27bcac10ba63e9f66185c53e124953bb76
tawnchan <tawnchan@gmail.com>
2022-10-13 10:49:43 -0700

    completed blog-update functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
 get 'posts' => 'post#index', as: 'posts'
+get 'posts/new' => 'post#new', as: 'new_post'
+get 'posts/:id' => 'post#show', as: 'post'
 post 'posts' => 'post#create'
 get 'posts/:id/edit' => 'post#edit', as: 'edit_post'
+patch 'posts/:id' => 'post#update'
 delete 'posts/:id' => 'post#destroy', as: 'delete_post'
-get 'posts/new' => 'post#new', as: 'new_post'
-get 'post/:id' => 'post#show', as: 'post'
 root "post#index"
 end
